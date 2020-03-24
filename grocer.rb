aacc20dc355b3b2c2725d37b32ef7c80fce3968d
Steven Parsons <steven.parsons.us@gmail.com>
2020-03-24 01:41:24 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -16,24 +16,24 @@ answer
   # Consult README for inputs and outputs
 end
 
-def consolidate_cart(cart)
-  consolidatedCart = []
-  n = 0 
-  while n < cart.count
-    name = cart[n][:item]
-    grocery = cart[n]
-    itemAlreadyInCart = find_item_by_name_in_collection(name, consolidatedCart)
-      if itemAlreadyInCart == nil
-        consolidatedCart << grocery
-        consolidatedCart.last[:count] = 1
-        n+=1
-      else #itemAlreadyInCart
-        itemAlreadyInCart[:count] += 1
-        n+=1
-      end
-  end
-  consolidatedCart
-end
+def consolidate_cart(cart) #combines like items from a 'cart', adds a 'count' of that item, and puts it in a new consolidated Cart
+  consolidatedCart = [] #create our new cart which is currently empty
+  item = 0 #this is the item we are selecting from the old cart, lets start with the first (which is zero)
+  while item < cart.count #run this next bit of code until we run out of items from the old cart (in that case go to line 34)
+    grocery = cart[item] #lets assigne the shortcut 'grocery' to the item we're currently holding from the old cart
+    name = cart[item][:item] #lets retrieve the coded :item-name of that grocery and give it a shortcut 'name'
+    itemAlreadyInCart = find_item_by_name_in_collection(name, consolidatedCart) #a shortcut searching the new cart for the item
+      if !itemAlreadyInCart #do the next 3 lines of code, "if" its "not true (= !)" the "item is already in the cart", else go to 30
+        grocery[:count] = 1 #that means its the '1st' of that 'item/grocery' from the old 'cart', we need to add that to a 'count'.
+        consolidatedCart << grocery #and add that new item/grocery to the new cart
+        item+=1 #now grab the next item in the old cart and go to line 33
+      else #do the next 2 lines of code if the 'item is Already In the Cart'
+        itemAlreadyInCart[:count] += 1 #since we already put one of this grocery in the new cart, we just have to add 1 to its count
+        item+=1 #then select the next item in out old cart and go to line 33
+      end #go to line 22 to see if this new item is already in our cart
+  end #if you are here, its because you have finished consolidating your Cart, go to the next line
+  consolidatedCart #lets now display what our complete cart looks like and finish the task
+end #all done
 
 def apply_coupons(cart, coupons)
   t = 0 
